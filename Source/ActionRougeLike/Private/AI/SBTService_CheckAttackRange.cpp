6990087ea2feabd9f5f60de8f48d5f4ecb458d8b
StewartMckenzie <mckenzie.stewart.e@gmail.com>
2022-04-11 20:50:07 -0700

    Updated AI

---
+++ Source/ActionRougeLike/Private/AI/SBTService_CheckAttackRange.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/SBTService_CheckAttackRange.h"
+#include "../../../../../../UESource/UnrealEngine/Engine/Source/Runtime/AIModule/Classes/BehaviorTree/BlackboardComponent.h"
+#include "../../../../../../UESource/UnrealEngine/Engine/Source/Runtime/AIModule/Classes/AIController.h"
+
+void USBTService_CheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+
+	//CHeck distance between ai pawn and target actor
+
+	UBlackboardComponent* BlackBoardComp = OwnerComp.GetBlackboardComponent();
+	if (ensure(BlackBoardComp)) {
+		AActor * TargetActor= Cast<AActor>(BlackBoardComp->GetValueAsObject("TargetActor"));
+		if (TargetActor) {
+			AAIController* MyController = OwnerComp.GetAIOwner();
+			if (ensure(MyController)) {
+				APawn* AIPawn = MyController->GetPawn();
+				if (ensure(AIPawn)) {
+					float DistanceTo =FVector::Distance(TargetActor->GetActorLocation(), AIPawn->GetActorLocation());
+
+					bool bWithinRange = DistanceTo < 2000.f;
+
+
+					bool bHasLOS =bWithinRange?MyController->LineOfSightTo(TargetActor):false;
+
+
+
+
+					BlackBoardComp->SetValueAsBool(AttackRangeKey.SelectedKeyName, (bWithinRange && bHasLOS));
+
+
+
+				}
+
+
+
+
+
+			}
+		}
+	}
+
+
+}
